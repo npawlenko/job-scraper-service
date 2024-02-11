825d1fa9debb40d47ceb892dccf6b60bb9c61e4b
Norbert Pawlenko <blinkz8@protonmail.com>
2024-02-11 18:51:50 +0100

    Initial commit

---
+++ src/main/java/com/github/npawlenko/jobscraper/jobscraper/JobScraperApplication.java
@@ -0,0 +1,13 @@
+package com.github.npawlenko.jobscraper.jobscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobScraperApplication.class, args);
+	}
+
+}
